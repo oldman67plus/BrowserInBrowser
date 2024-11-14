6eee744659088cf1e5a648a8cce5f7de5ca341f6
oldman67plus <nathan2pyt@gmail.com>
2024-11-14 12:17:21 -0800

    Added files to try and use for this repo
    Got these files from jakestrouse00

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function uwebsite() {
+    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://bing.com\' height=\'100%\' width=\'100%\'></iframe>');
+
+}
+
+function uvideo() {
+    var thevalue = document.getElementById("thevideo").value;
+    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://ytunblock.glitch.me/downloadvid?v='+thevalue+'\' height=\'100%\' width=\'100%\'></iframe>');
+
+}
+
+function usweb() {
+    var thevalue = document.getElementById("thesite").value;
+    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://'+thevalue+'\' height=\'100%\' width=\'100%\'></iframe>');
+
+}
