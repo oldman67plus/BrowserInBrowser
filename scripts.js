8d140381fcbe9eb47f3407a0b063af98bf9f10c0
oldman67plus <nathan2pyt@gmail.com>
2024-11-15 10:30:10 -0800

    testing

--- scripts.js
+++ scripts.js
@@ -1,10 +1,23 @@
-function uwebsite() {
-    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://www.google.com\' height=\'100%\' width=\'100%\'></iframe>');
+function usweb() {
+  const thevalue = document.getElementById("thesite").value;
 
+  // Basic URL validation and sanitization
+  if (!isValidURL(thevalue)) {
+    alert("Please enter a valid URL.");
+    return;
+  }
+
+  // Create a new iframe element
+  const iframe = document.createElement('iframe');
+  iframe.src = thevalue;
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+
+  // Append the iframe to the document body
+  document.body.appendChild(iframe);
 }
 
-function usweb() {
-    var thevalue = document.getElementById("thesite").value;
-    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://'+thevalue+'\' height=\'100%\' width=\'100%\'></iframe>');
-
+function isValidURL(urlString) {
+  var urlPattern = /^(http|https):\/\/[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}(:[0-9]+)?(\/.*)?$/;
+  return urlPattern.test(urlString);
 }
