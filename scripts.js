a05b86e3279465c80949610e39e94f0e68477154
oldman67plus <nathan2pyt@gmail.com>
2024-11-15 14:50:05 -0800

    no Diddy allow

--- scripts.js
+++ scripts.js
@@ -4,6 +4,22 @@ function uwebsite() {
 }
 
 function usweb() {
-    var thevalue = document.getElementById("thesite").value;
-    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://'+thevalue+'\' height=\'100%\' width=\'100%\'></iframe>');
+  var thevalue = document.getElementById("thesite").value;
+
+  // Create a new iframe element
+  var iframe = document.createElement('iframe');
+  iframe.src = thevalue;
+  iframe.style.width = '100%';
+  iframe.style.height = '100%';
+
+  // Add an event listener to prevent navigation
+  iframe.addEventListener('load', function() {
+    iframe.contentWindow.addEventListener('beforeunload', function(e) {
+      e.preventDefault();
+      e.returnValue = '';
+    });
+  });
+
+  // Append the iframe to the document body
+  document.body.appendChild(iframe);
 }
