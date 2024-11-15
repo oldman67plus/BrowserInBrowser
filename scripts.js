b965b0e8df12fddc8cca7b2a0d3f5339642a8b99
oldman67plus <nathan2pyt@gmail.com>
2024-11-15 14:56:12 -0800

    no diddy work again

--- scripts.js
+++ scripts.js
@@ -4,22 +4,7 @@ function uwebsite() {
 }
 
 function usweb() {
-  var thevalue = document.getElementById("thesite").value;
-
-  // Create a new iframe element
-  var iframe = document.createElement('iframe');
-  iframe.src = thevalue;
-  iframe.style.width = '100%';
-  iframe.style.height = '100%';
-
-  // Add an event listener to prevent navigation
-  iframe.addEventListener('load', function() {
-    iframe.contentWindow.addEventListener('beforeunload', function(e) {
-      e.preventDefault();
-      e.returnValue = '';
-    });
-  });
-
-  // Append the iframe to the document body
-  document.body.appendChild(iframe);
+    var thevalue = document.getElementById("thesite").value;
+  const fixedUrl = 'https://'+thevalue+'\';
+  document.write('<style>*{margin:0;border:0;}</style><iframe src=\'' + fixedUrl + '\' height=\'100%\' width=\'100%\'></iframe>');
 }
