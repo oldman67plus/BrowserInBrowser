3df5a8c1c78bcd81e442cba1a103230969b516ba
oldman67plus <nathan2pyt@gmail.com>
2024-12-09 15:08:58 -0800

    hpe

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
-function uwebsite() {
-    document['write']('<style>*{margin:0;border:0;}</style><iframe src=\'https://www.google.com\' height=\'100%\' width=\'100%\'></iframe>');
-
 }
 
 function usweb() {
-    var website = document.getElementById("thesite").value;
-  var const fixedUrl = 'https://+website+/';
-  document.write('<style>*{margin:0;border:0;}</style><iframe src=\'+fixedUrl+\' height=\'100%\' width=\'100%\'></iframe>');
+  const website = document.getElementById("thesite").value;
+  const url = new URL(website, window.location.origin);
+
+  // Open the website in a new window or tab
+  window.open(url.toString(), '_blank');
 }
